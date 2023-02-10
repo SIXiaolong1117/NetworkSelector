1e63dbe2e774685d76f0c6b0042ae22b66565aaf
Direct5dom <sixiaolong2018@outlook.com>
2023-02-10 19:42:55 +0800

    添加项目文件。

---
+++ NetworkSelector/NetSetting.xaml.cs
@@ -0,0 +1,102 @@
+﻿// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace NetworkSelector
+{
+    public sealed partial class NetSetting : Page
+    {
+        ApplicationDataContainer localSettings = ApplicationData.Current.LocalSettings;
+        public List<string> ConfigSelector { get; } = new List<string>()
+        {
+            "预设1",
+            "预设2",
+            "预设3",
+            "预设4",
+            "预设5"
+        };
+        public void saveConfig()
+        {
+
+            localSettings.Values[(configName.SelectedItem as string) + "netName"] = netName.Text;
+            localSettings.Values[(configName.SelectedItem as string) + "IPAddr"] = IPAddr.Text;
+            localSettings.Values[(configName.SelectedItem as string) + "mask"] = mask.Text;
+            localSettings.Values[(configName.SelectedItem as string) + "gateway"] = gateway.Text;
+            localSettings.Values[(configName.SelectedItem as string) + "DNS1"] = DNS1.Text;
+            localSettings.Values[(configName.SelectedItem as string) + "DNS2"] = DNS2.Text;
+        }
+        public void applyConfig()
+        {
+            localSettings.Values["netshCMD"] =
+                "netsh interface ip set address name='" + netName.Text + "' source=static addr='" + IPAddr.Text + "' mask='" + mask.Text + "' gateway='" + gateway.Text + "'; "
+                + "netsh interface ip set dns name='" + netName.Text + "' source=static addr='" + DNS1.Text + "' register=primary;"
+                + "netsh interface ip add dns name='" + netName.Text + "' addr='" + DNS2.Text + "' index=2;";
+            netshCMD.Text = localSettings.Values["netshCMD"] as string;
+        }
+        public NetSetting()
+        {
+            this.InitializeComponent();
+
+            netName.Text = localSettings.Values[(configName.SelectedItem as string) + "netName"] as string;
+            IPAddr.Text = localSettings.Values[(configName.SelectedItem as string) + "IPAddr"] as string;
+            mask.Text = localSettings.Values[(configName.SelectedItem as string) + "mask"] as string;
+            gateway.Text = localSettings.Values[(configName.SelectedItem as string) + "gateway"] as string;
+            DNS1.Text = localSettings.Values[(configName.SelectedItem as string) + "DNS1"] as string;
+            DNS2.Text = localSettings.Values[(configName.SelectedItem as string) + "DNS2"] as string;
+
+            configName.SelectedItem = ConfigSelector[0];
+        }
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+            saveConfig();
+        }
+        private void saveApplyButton_Click(object sender, RoutedEventArgs e)
+        {
+            saveConfig();
+            applyConfig();
+            //ThreadStart childref = new ThreadStart(saveApplyChildThread);
+            //Thread childThread = new Thread(childref);
+            //childThread.Start();
+            Process process = new Process();
+            process.StartInfo.FileName = "PowerShell.exe";
+            process.StartInfo.Arguments = localSettings.Values["netshCMD"] as string;
+            //是否使用操作系统shell启动
+            process.StartInfo.UseShellExecute = true;
+            //是否在新窗口中启动该进程的值 (不显示程序窗口)
+            process.StartInfo.CreateNoWindow = true;
+            process.Start();
+            process.WaitForExit();
+            process.Close();
+        }
+        public void saveApplyChildThread()
+        {
+        }
+        private void configName_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            netName.Text = localSettings.Values[(configName.SelectedItem as string) + "netName"] as string;
+            IPAddr.Text = localSettings.Values[(configName.SelectedItem as string) + "IPAddr"] as string;
+            mask.Text = localSettings.Values[(configName.SelectedItem as string) + "mask"] as string;
+            gateway.Text = localSettings.Values[(configName.SelectedItem as string) + "gateway"] as string;
+            DNS1.Text = localSettings.Values[(configName.SelectedItem as string) + "DNS1"] as string;
+            DNS2.Text = localSettings.Values[(configName.SelectedItem as string) + "DNS2"] as string;
+        }
+    }
+}
---
+++ NetworkSelector/SettingsPage.xaml.cs
@@ -0,0 +1,99 @@
+// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace NetworkSelector
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class SettingsPage : Page
+    {
+        // 启用本地设置数据
+        ApplicationDataContainer localSettings = ApplicationData.Current.LocalSettings;
+
+        // 材料ComboBox列表List
+        public List<string> material { get; } = new List<string>()
+        {
+            "Mica",
+            "Acrylic"
+        };
+
+        // 页面初始化
+        public SettingsPage()
+        {
+            // 初始化
+            this.InitializeComponent();
+
+            //backgroundMaterial.PlaceholderText = localSettings.Values["materialStatus"] as string;
+            // 读取本地设置数据，调整ComboBox状态
+            if (localSettings.Values["materialStatus"] as string == "Mica")
+            {
+                backgroundMaterial.SelectedItem = material[0];
+            }
+            else if (localSettings.Values["materialStatus"] as string == "Acrylic")
+            {
+                backgroundMaterial.SelectedItem = material[1];
+            }
+            else
+            {
+                // 非法输入，设置默认材料为Mica
+                localSettings.Values["materialStatus"] = "Mica";
+                backgroundMaterial.SelectedItem = material[0];
+                // 非法输入，扔出警报
+                //throw new Exception($"Wrong material type: {localSettings.Values["materialStatus"]}");
+            }
+        }
+
+        // 背景材料设置ComboBox改动事件
+        private void backgroundMaterial_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string materialStatus = e.AddedItems[0].ToString();
+            switch (materialStatus)
+            {
+                case "Mica":
+                    if (localSettings.Values["materialStatus"] as string != "Mica")
+                    {
+                        localSettings.Values["materialStatus"] = "Mica";
+                        Microsoft.Windows.AppLifecycle.AppInstance.Restart("");
+                    }
+                    else
+                    {
+                        localSettings.Values["materialStatus"] = "Mica";
+                    }
+                    break;
+                case "Acrylic":
+                    if (localSettings.Values["materialStatus"] as string != "Acrylic")
+                    {
+                        localSettings.Values["materialStatus"] = "Acrylic";
+                        Microsoft.Windows.AppLifecycle.AppInstance.Restart("");
+                    }
+                    else
+                    {
+                        localSettings.Values["materialStatus"] = "Acrylic";
+                    }
+                    break;
+                default:
+                    throw new Exception($"Invalid argument: {materialStatus}");
+            }
+        }
+    }
+}
