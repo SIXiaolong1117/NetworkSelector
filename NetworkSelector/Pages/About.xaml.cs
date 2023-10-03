1817acb473889e156f3691a877eda4f4204afff2
Direct5dom <sixiaolong2018@outlook.com>
2023-10-04 00:35:32 +0800

    添加赞助感谢页面。

--- NetworkSelector/Pages/About.xaml.cs
+++ NetworkSelector/Pages/About.xaml.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation and Contributors.
 // Licensed under the MIT License.
 
+using ABI.System;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -8,15 +9,20 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Data.SqlTypes;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using System.Xml.Linq;
 using Windows.ApplicationModel;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Gaming.Preview.GamesEnumeration;
 
 namespace NetworkSelector.Pages
 {
@@ -31,7 +37,7 @@ namespace NetworkSelector.Pages
             var version = package.Id.Version;
 
             APPVersion.Content = $"{version.Major}.{version.Minor}.{version.Build}.{version.Revision}";
-            APPVersion.NavigateUri = new Uri($"https://github.com/Direct5dom/NetworkSelector/releases/tag/{version.Major}.{version.Minor}.{version.Build}.{version.Revision}");
+            APPVersion.NavigateUri = new System.Uri($"https://github.com/Direct5dom/NetworkSelector/releases/tag/{version.Major}.{version.Minor}.{version.Build}.{version.Revision}");
         }
     }
 }
