856a54753c757ed58f9ad5b18c9935db031a65b9
SI Xiaolong <SIXiaolong_GitHub@outlook.com>
2024-04-10 08:31:01 +0800

    升级软件包。

--- NetworkSelector/Pages/About.xaml.cs
+++ NetworkSelector/Pages/About.xaml.cs
@@ -1,37 +1,18 @@
 ﻿// Copyright (c) Microsoft Corporation and Contributors.
 // Licensed under the MIT License.
 
-using ABI.System;
-using Microsoft.UI.Dispatching;
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Newtonsoft.Json.Linq;
+﻿using Microsoft.UI.Xaml.Controls;
 using System;
-using System.Collections.Generic;
-using System.Data.SqlTypes;
-using System.IO;
-using System.Linq;
-using System.Net.Http;
-using System.Runtime.InteropServices.WindowsRuntime;
-using System.Threading;
 using System.Threading.Tasks;
-using System.Xml.Linq;
 using Windows.ApplicationModel;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.Gaming.Preview.GamesEnumeration;
-using Windows.Media.Protection.PlayReady;
+using System.Net.Http;
+using Microsoft.UI.Xaml;
 
 namespace NetworkSelector.Pages
 {
     public sealed partial class About : Page
     {
-        private DispatcherQueue _dispatcherQueue;
         public About()
         {
             this.InitializeComponent();
@@ -40,14 +21,11 @@ namespace NetworkSelector.Pages
             var package = Package.Current;
             var version = package.Id.Version;
 
-            // 获取UI线程的DispatcherQueue
-            _dispatcherQueue = DispatcherQueue.GetForCurrentThread();
-
             APPVersion.Text = $"{version.Major}.{version.Minor}.{version.Build}";
             //APPVersion.NavigateUri = new System.Uri($"https://github.com/Direct5dom/NetworkSelector/releases/tag/{version.Major}.{version.Minor}.{version.Build}.{version.Revision}");
             //APPVersion.NavigateUri = new System.Uri($"https://www.microsoft.com/store/apps/9PDQC93R0WLF");
 
-            GetSponsorList();
+            GetList();
         }
         private void AboutAliPay_Click(object sender, RoutedEventArgs e)
         {
@@ -57,62 +35,74 @@ namespace NetworkSelector.Pages
         {
             AboutWePayTips.IsOpen = true;
         }
-        private void GetSponsorList()
+        private async Task<string> HTTPResponse(string http)
         {
-            // 在子线程中执行任务
-            Thread subThread = new Thread(new ThreadStart(async () =>
+            using (HttpClient client = new HttpClient())
             {
-                string nameList = null;
-                using (HttpClient client = new HttpClient())
+                HttpResponseMessage response = await client.GetAsync(http);
+                if (response.IsSuccessStatusCode)
                 {
-                    // 发起GET请求以获取文件内容
-                    // 首先尝试从GitHub获取数据
-                    try
-                    {
-                        HttpResponseMessage response = await client.GetAsync($"https://raw.githubusercontent.com/Direct5dom/Direct5dom/main/README/Sponsor/List");
-                        if (response.IsSuccessStatusCode)
-                        {
-                            // 从GitHub的响应中读取文件内容
-                            nameList = await response.Content.ReadAsStringAsync();
-                        }
-                        else
-                        {
-                            nameList = "Try Gitee";
-                        }
-                    }
-                    catch
-                    {
-                        nameList = "Try Gitee";
-                    }
-
-                    // 如果GitHub通信失败，尝试从Gitee获取数据
-                    if (nameList == "Try Gitee")
-                    {
-                        try
-                        {
-                            HttpResponseMessage response = await client.GetAsync($"https://gitee.com/XiaolongSI/Direct5dom/raw/main/README/Sponsor/List");
-                            if (response.IsSuccessStatusCode)
-                            {
-                                // 从Gitee的响应中读取文件内容
-                                nameList = await response.Content.ReadAsStringAsync();
-                            }
-                            else
-                            {
-                                nameList = "无法连接至 Github 或 Gitee 获取赞助者名单。(0)";
-                            }
-                        }
-                        catch
-                        {
-                            nameList = "无法连接至 Github 或 Gitee 获取赞助者名单。(1)";
-                        }
-                    }
+                    // 从GitHub的响应中读取文件内容
+                    return await response.Content.ReadAsStringAsync();
                 }
-                _dispatcherQueue.TryEnqueue(() =>
+                else
                 {
-                    NameList.Text = nameList;
-                });
-            }));
-            subThread.Start();
+                    return "";
+                }
+            }
+        }
+        private async void GetList()
+        {
+            string nameList = null;
+            string stringList = null;
+            try
+            {
+                nameList = await HTTPResponse("https://raw.githubusercontent.com/Direct5dom/Direct5dom/main/README/Sponsor/List");
+            }
+            catch (Exception ex)
+            {
+                try
+                {
+                    nameList = await HTTPResponse("https://gitee.com/XiaolongSI/Direct5dom/raw/main/README/Sponsor/List");
+                }
+                catch (Exception ex2)
+                {
+                    nameList = "无法连接至 Github 或 Gitee。";
+                }
+            }
+            try
+            {
+                stringList = await HTTPResponse("https://raw.githubusercontent.com/Direct5dom/Direct5dom/main/README/Text/List");
+            }
+            catch (Exception ex)
+            {
+                try
+                {
+                    stringList = await HTTPResponse("https://gitee.com/XiaolongSI/Direct5dom/raw/main/README/Text/List");
+                }
+                catch (Exception ex2)
+                {
+                    stringList = "";
+                }
+            }
+
+            string randomLine = null;
+            try
+            {
+                // 使用换行符分割字符串成数组
+                string[] lines = stringList.Split(new[] { "\n", "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+                // 使用随机数生成器生成一个随机索引
+                Random rand = new Random();
+                int randomIndex = rand.Next(0, lines.Length);
+
+                // 随机选择一个字符串
+                randomLine = lines[randomIndex];
+            }
+            catch (Exception ex) { }
+
+            NameList.Text = nameList;
+            TipsTips.Text = randomLine;
         }
     }
 }
